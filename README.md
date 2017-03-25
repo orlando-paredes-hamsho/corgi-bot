7bc42ebdc857916b1077c3eb1994063a86251232
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-03-25 15:49:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Corgi-Bot
+
+I'm a corgi bot written with [Twitter Bot Bootstrap](https://github.com/spences10/twitter-bot-bootstrap).
+
+## Follow Me?
+I live at https://twitter.com/AboutCorgis, I retweet everything corgi every 30 mins or so.
+
+## Why was I made?
+I was made as a part of the 100 days of code challenge, but I intend to stay ;)
+
+
---
+++ src/helpers/strings.js
@@ -0,0 +1,55 @@
+module.exports = {
+  queryString: [
+    'corgi',
+    'corgis',
+    '#AllAboutCorgis'
+  ],
+  queryStringSubQuery: [
+    ' pembroke',
+    ' cardigan',
+    ' puppy',
+    ' dog',
+    ' dogs'
+  ],
+  blockedStrings: [
+    'sex',
+    'porn',
+    'gore',
+    'accident',
+    'sexy',
+    'shit',
+    'fuck',
+    'cunt',
+    'ass',
+    'dick',
+    'pussy',
+    'damn',
+    'bitch',
+    'crap',
+    'piss',
+    'dick',
+    'asshole',
+    'fag',
+    'bastard',
+    'douche',
+    'slut',
+    'arsehole',
+    'bloody'
+  ],
+  resultType: [
+    'mixed',
+    'recent',
+    'popular'
+  ],
+  responseString: [
+    'BARK BARK RUFF! (I\'ve been informed that you follow me @screenName, blessings be onto you)',
+    'BARK! (I welcome thee into my twitter, @screenName)',
+    '@screenName WOOF WOOF WOOF! (I appreciate your kindness, thank you)',
+    '*Howls for about 1 minute* (Thank you @screenName)',
+    '*wiggles tail* (It\'s a beautiful day to be followed by you @screenName, you have my thanks)',
+    'Kindred spirits be upon you @screenName *coughs* I mean WOOF!',
+    'RUFF! @screenName',
+    '@screenName *derps uncontrollably, then normalizes and wiggles tail*',
+    '@screenName *achieves sentience due to your following, then gets distracted by fly*'
+  ]
+}
